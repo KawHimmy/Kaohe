150f40eeefa90ba08a6ab5ebc7d075dfbd9d1eac
Linxy2004 <136216354+Linxy2004@users.noreply.github.com>
2023-10-04 21:53:03 +0800

    第四周学习总结
    1.学习了mybatis技术
    2.学习了spring技术
    3.初步了解了springMVC技术，初步了解了ssm框架的整合
    4.初步学习了springboot技术

---
+++ SpringBoot_Demo/src/main/java/com/example/springboot_demo/Controller/BookController.java
@@ -0,0 +1,18 @@
+package com.example.springboot_demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping("/{id}")
+    public String getById(@PathVariable Integer id){
+        System.out.println("id ==> "+id);
+        return "hello , spring boot!";
+    }
+
+}
